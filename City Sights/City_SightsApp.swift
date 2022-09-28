b841325c44068f5377500db7d1bcadf5bf6a2680
Semih Cetin <semihcettin@gmail.com>
2022-09-28 11:11:28 +0300

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by Semih Cetin on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
